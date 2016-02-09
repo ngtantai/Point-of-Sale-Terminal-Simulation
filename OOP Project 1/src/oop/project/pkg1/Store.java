ff099e1c11ba3187cce6495026e797784651f7ab
Jose  Ortiz <joseortizcosta@icloud.com>
2016-02-09 14:38:23 -0800

    Created catalog object to the constructor

--- OOP Project 1/src/oop/project/pkg1/Store.java
+++ OOP Project 1/src/oop/project/pkg1/Store.java
@@ -4,76 +4,71 @@
  * and open the template in the editor.
  */
 package oop.project.pkg1;
+
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import Catalog.*; // Imports catalog package
+
 /**
  *
  * @author mac
  */
 public class Store {
+
     public String storeName;
     private int option;
     public Post currentPost;
-    
-    public Store(String name){
+    private Stock catalog; // catalog instance
+
+    public Store(String name) {
         //Initialize stuff here needs to read in data and what not
         this.storeName = name;
         //First check and see if this store has already been created
-        
-        
-        
+        catalog = new Stock(); // creates an object of the Stock class
+        catalog.loadCatalog(); // loads catalog of products in memory
+        catalog.viewCatalog();
         System.out.println("Welcome");
-        
-        
-        
-        
-        
-        
+
     }
-    
-    
-    
-    
-    public void runStore(){
+
+    public void runStore() {
         Scanner sc = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.println("***STORE MENU***");
             System.out.println("Options : (Enter a number)");
             System.out.println("1: Exit");
             System.out.println("2: Start Post");
             System.out.println("3: Product Manager");
             System.out.println("4: Transaction Manager");
-            
+
             option = sc.nextInt();
-            if(option == 1){
+            if (option == 1) {
                 //Exit the system
                 System.out.println("Goodbye!");
                 break;
-            }else if(option == 2){
+            } else if (option == 2) {
                 //Initialize the post
                 currentPost = new Post(this);
                 currentPost.runPost();
                 //won't need the same object again, okay to get rid of it
                 currentPost = null;
-                
-            }else{
+
+            } else {
                 System.out.println("Invalid Option");
             }
         }
     }
-    
-    public void test(){
+
+    public void test() {
         System.out.println("test");
     }
-    
 
-    public static void createStore(String option){
+    public static void createStore(String option) {
         //Create directory and files
     }
-    
-   
-    
+
 }
